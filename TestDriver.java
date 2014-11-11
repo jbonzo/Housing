abff016c688273aae9ed1e92418aaafb56de301c
jbonzo <rbarillas3@gatech.edu>
2014-11-11 16:50:08 -0500

    idk

--- TestDriver.java
+++ TestDriver.java
@@ -1,6 +1,6 @@
 public class TestDriver {
     public static void main(String[] args) {
-        MySortedSet<Brother> set = CSVReader.readAndAdd("C:/Users/Ricky/Desktop/Projects/House/Responses.csv").sort(
+        MySortedSet<Brother> set = CSVReader.readAndAdd("Responses.csv").sort(
                                     (a, b) -> a.compareTo(b));
         System.out.println(set);
 
